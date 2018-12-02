faacec617a88bc738942c82a03a040aef70b4ec5
61131114002 <43134332+61131114002@users.noreply.github.com>
2018-12-02 12:43:38 +0700

    Car2

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1543729083,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/bg-sheet0.png",
+		"images/hero-sheet0.png",
+		"images/hero-sheet1.png",
+		"images/solid-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/car-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/ok-sheet0.png",
+		"images/real-sheet0.png",
+		"images/real-sheet1.png",
+		"images/real-sheet2.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/start-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
